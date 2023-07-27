52a77a4b27d9e3dea46e39c36f4a5d9d2cfaf9ac
MOIS3Y <s.zhukovskii@ispsystem.com>
2023-07-27 11:32:02 +0900

    Fix: urlpatterns download view vars

--- logs_collector/collector/urls.py
+++ logs_collector/collector/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index',),
     path('test/<str:path>/', views.test_page, name='test_page'),
-    path('archives/<ticket>/<archive>', views.download, name="download")
+    path('archives/<ticket>/<file>', views.download, name="download")
 ]
